fa7bdfe3a13d7f7e650edeb253104b3af03ecb69
skoh-fley <skoh.fley@gmail.com>
2011-03-12 17:51:24 -0800

    general code cleanups and whitespace added

--- admin/index.php
+++ admin/index.php
@@ -30,40 +30,59 @@ session_start();
 
 // logout
 if(isset($_GET['logout'])) {
+
   unset($_SESSION['admin']);
   session_destroy();
   header("Location: ../");
+
 }
 
 // login
 if($_SESSION['admin'] != $config['adminpass']) {
+
   if(isset($_POST['login'])) {
+
     if(md5($_POST['password']) == $config['adminpass']) {
+
       $_SESSION['admin'] = $config['adminpass'];
       header("Location: ./");
+
     } else {
+
       runtemplate("admin_header");
       print("<div style=\"position: absolute; left: 400px; top: 180px;\">");
       error("Invalid password.", false);
+
     }
-  } else runtemplate("admin_header");
+
+  } else {
+
+    runtemplate("admin_header");
+
+  }
   
   print("<div style=\"position: absolute; left: 400px; top: 200px;\">");
+
   if(isset($_SESSION['admin'])) // bad session
     print("<span class=\"error\">Session invalid; please login again.</span><br />");
+
   print("<span class=\"sitetitle\">administrate</span> <span class=\"sitesub\">" . $config['blog_name'] . "</span><br /><br />");
   print("<form action=\"./\" method=\"post\">");
   print("<a type=\"blog_title\">enter password</a><br /><input type=\"password\" name=\"password\">");
   print("&nbsp;&nbsp;&nbsp;&nbsp;<input type=\"submit\" name=\"login\" value=\"login\"></form>");
   
   exit(); // don't allow any further access of administration unless logged in
+
 }
 
 runtemplate("admin_header");
 
 if(isset($_GET['config'])) {
+
   print("<span class=\"pagesub\">config</span><br /><br />\n");
+
   if(isset($_POST['config_submit'])) {
+
     $options = array(
       'blog_name'     => $_POST['blog_name'],
       'blog_sub'      => $_POST['blog_sub'],
@@ -74,10 +93,14 @@ if(isset($_GET['config'])) {
       'abcposts'      => $_POST['abcposts'],
       'showadmin'     => $_POST['showadmin']
     );
+
     if(!write_config($options))
       exit("<span class=\"error\">Couldn't write to <tt>config.php</tt>; check permissions and try again.</span>");
+
     print("<span class=\"success\">Configuration saved.</span>");
+
   } else {
+
   ?>
   <form action="?config" method="post">
   blog name<br /><input type="text" name="blog_name" value="<?php print($config['blog_name']); ?>" class="form_text"><br /><br />
@@ -105,47 +128,65 @@ if(isset($_GET['config'])) {
   </select> show admin panel link in sidebar<br /><br />
   <input type="submit" name="config_submit" value="save" class="form_submit">
 
-<?php
-  }
-}
+  <?php
 
-elseif(isset($_GET['plugins'])) {
+  }
+
+} elseif(isset($_GET['plugins'])) {
+
   print("<span class=\"pagesub\">plugins</span><br />\n");
   print("<div style=\"position: relative; left: 8px;\">");
   plug("admplugins", "listing");
   print("</div>");
-}
 
-elseif(isset($_GET['plugin'])) {
+} elseif(isset($_GET['plugin'])) {
+
   // blank page for plugins to use as a config/about page
   plug("admplugins", "page");
-}
 
-elseif(isset($_GET['templates'])) {
+} elseif(isset($_GET['templates'])) {
+
   print("<span class=\"pagesub\">templates</span><br /><br />\n");
+
   if(isset($_POST['template_submit'])) {
+
     if(!write_config(array("template" => $_POST['template'])))
       exit("<span class=\"error\">Error writing to <tt>config.php</tt>. Check permissions and try again.</span>");
+
     print("<span class=\"success\">Template changed.</span>");
+
   } else {
+
     print("<form action=\"?templates\" method=\"post\">\n");
     $templates = glob($root . "templates/*", GLOB_ONLYDIR);
+
     foreach($templates as $template) {
+
       $template = str_replace($root . "templates/", "", $template);
       print("<input type=\"radio\" name=\"template\" value=\"$template\"");
-      if($config['template'] == $template) print(" checked");
-      print("> <tt>$template</tt><br />\n");
-    }
-    print("<br /><input type=\"submit\" name=\"template_submit\" value=\"save\" class=\"form_submit\"></form>");
-  }
-}
 
-elseif(isset($_GET['create'])) {
+      if($config['template'] == $template)
+        print(" checked");
+
+      print("> <tt>$template</tt><br />\n");
+
+    }
+
+    print("<br /><input type=\"submit\" name=\"template_submit\" value=\"save\" class=\"form_submit\"></form>");
+
+  }
+
+} elseif(isset($_GET['create'])) {
+
   print("<span class=\"pagesub\">create</span><br /><br />\n");
+
   if(isset($_POST['submit_post'])) {
+
     if(create_entry($_POST['title'], $_POST['content'], $_POST['type']))
       print("<span class=\"success\">Entry created.</span>");
+
   } else {
+
     plug("admcreate", "top");
     print("<form action=\"?create\" method=\"post\">\n");
     print("title<br /><input class=\"form_text\" name=\"title\" size=\"50\" type=\"text\"><br><br>\n");
@@ -159,34 +200,54 @@ elseif(isset($_GET['create'])) {
     print("<input class=\"form_submit\" name=\"submit_post\" type=\"submit\" value=\"post\">\n");
     plug("admcreate", "button_after");
     print("</form>\n\n");
-  }
-}
 
-elseif(isset($_GET['modify'])) {
+  }
+
+} elseif(isset($_GET['modify'])) {
+
   print("<span class=\"pagesub\">modify</span><br /><br />\n");
+
   if($_GET['modify'] != null) {
+
     if(isset($_POST['modify_post'])) {
+
       $oldname = $_POST['oldfile'];
+
       if(strstr($oldname, "docs/")) {
+
         $type = "docs";
         $oldname = str_replace("docs/", "", $oldname);
+
       } elseif(strstr($oldname, "posts/")) {
+
         $type = "posts";
         $oldname = str_replace("posts/", "", $oldname);
+
       }
+
       if(!delete_entry($oldname, $type))
         exit("<span class=\"error\">Old entry could not be removed. Check permissions and try again.</span>");
+
       if(create_entry($_POST['title'], $_POST['content'], $_POST['type']))
         print("<span class=\"success\">Entry modified.</span>");
+
     } else {
+
       if(substr($_GET['modify'], 0, 5) == "posts") {
+
         $oldtype = "posts";
         $oldtitle = str_replace("posts/", "", $_GET['modify']);
+
       } elseif(substr($_GET['modify'], 0, 4) == "docs") {
+
         $oldtype = "docs";
         $oldtitle = str_replace("docs/", "", $_GET['modify']);
+
+      } else {
+
+        exit("<span class=\"error\">Bad entry type.</span>");
+
       }
-      else exit("<span class=\"error\">Bad entry type.</span>");
       
       $oldtitle = deparse_title($oldtitle);
       $oldcontent = file_get_contents($root . $_GET['modify'] . ".txt");
@@ -203,7 +264,10 @@ elseif(isset($_GET['modify'])) {
       print("name=\"type\" type=\"radio\" value=\"posts\">post\n");
       
       print("<input ");
-      if($oldtype == "docs") print("checked ");
+      
+      if($oldtype == "docs")
+        print("checked ");
+
       print("name=\"type\" type=\"radio\" value=\"docs\">doc\n");
       
       plug("admmodify", "type_after");
@@ -212,8 +276,11 @@ elseif(isset($_GET['modify'])) {
       print("<input class=\"form_submit\" name=\"modify_post\" type=\"submit\" value=\"modify\">\n");
       plug("admmodify", "button_after");
       print("</form>\n\n");
+
     }
+
   } else {
+
     $posts = glob($root . "posts/*.txt");
     $docs = glob($root . "docs/*.txt");
     
@@ -224,17 +291,21 @@ elseif(isset($_GET['modify'])) {
     $docstr = "";
     
     foreach($posts as $post) {
+
       $post = str_replace("../posts/", "", $post);
       $post = str_replace(".txt", "", $post);
       $post_title = deparse_title($post);
       $poststr .= "&nbsp;&nbsp;<a href=\"?del=posts/$post\" class=\"small\">[del]</a>&nbsp;<a href=\"?modify=posts/$post\">$post_title</a><br />";
+
     }
     
     foreach($docs as $doc) {
+
       $doc = str_replace("../docs/", "", $doc);
       $doc = str_replace(".txt", "", $doc);
       $doc_title = deparse_title($doc);
       $docstr .= "&nbsp;&nbsp;<a href=\"?del=docs/$doc\" class=\"small\">[del]</a>&nbsp;<a href=\"?modify=docs/$doc\">$doc_title</a><br />";
+
     }
     
     $poststr = str_replace("'", "\\'", $poststr);
@@ -258,52 +329,67 @@ if(paneElement.innerHTML == '')
 </script>
 
 <?php
-  }
-}
 
-elseif(isset($_GET['del'])) {
+  }
+} elseif(isset($_GET['del'])) {
+
   if(strstr($_GET['del'], "docs/")) {
+
     $type = "doc";
     $title = str_replace("docs/", "", $_GET['del']);
+
   } elseif(strstr($_GET['del'], "posts/")) {
+
     $type = "post";
     $title = str_replace("posts/", "", $_GET['del']);
+
   }
+
   if(isset($_POST['confirm_delete'])) {
+
     if(delete_entry($title, $type))
       print("<span class=\"success\">Entry deleted.</span>");
     else
       print("<span class=\"error\">Couldn't delete $type <tt>" . deparse_title($title) . "</tt>. Check permissions and try again.</span>");
+
   } else {
+
     print("<span class=\"pagesub\">delete entry</span><br /><br />\n");
     print("Are you sure you want to delete the $type <b><tt>" . deparse_title($title) . "</tt></b>? This cannot be undone.<br /><br />\n");
     print("<div align=\"right\"><form action=\"?del=" . $_GET['del'] . "\" method=\"post\"><input type=\"submit\" name=\"confirm_delete\" value=\"Yes, delete this " . $type . "\"></form>\n");
     print("<a href=\"?modify\" class=\"navitem\">Go back</a></div>");
-  }
-}
 
-elseif(isset($_GET['password'])) {
+  }
+
+} elseif(isset($_GET['password'])) {
+
   print("<span class=\"pagesub\">change password</span><br /><br />\n");
+
   if(isset($_POST['pass_submit'])) {
+
     if($_POST['newpass1'] != $_POST['newpass2'] || $_POST['newpass1'] == "")
       exit("Passwords did not match or were not entered. <a href=\"?password\">Try again</a>.");
     if(md5($_POST['curpass']) != $config['adminpass'])
       exit("Incorrect current password. <a href=\"?password\">Try again</a>.");
     if(!write_config(array("adminpass" => md5($_POST['newpass1']))))
       exit("<span class=\"error\">Error writing to <tt>config.php</tt>. Check permissions and try again.</span>");
+
     print("<span class=\"success\">Password changed.</span>");
+
   } else {
+
 ?>
   <form action="?password" method="post">
   current password<br /><input type="password" name="curpass" class="form_text"><br /><br />
   new password<br /><input type="password" name="newpass1" class="form_text"><br /><br />
   confirm<br /><input type="password" name="newpass2" class="form_text"><br /><br />
   <input type="submit" name="pass_submit" value="change password" class="form_submit"></form>
-<?php 
-  }
-}
 
-else { // main
+<?php 
+
+  }
+} else { // main
+
 ?>
 <p>&gt;&gt; <a href="?config" class="pagesub">config</a> &bull;
 change site options + variables</p>
@@ -324,7 +410,11 @@ destroy your administration session and return to your blog</p>
 <br />
 <p>&lt;&lt; <a href="../" class="pagesub">back to site</a> &bull;
 return to your blog</p>
+
 <?php
+
 }
+
 runtemplate("admin_footer");
+
 ?>
--- index.php
+++ index.php
@@ -42,43 +42,59 @@ $root = "./"; // so functions.php knows where we are
 /***** PREPERATIONS *********************************************************************/
 
 /***** CHECK FOR INSTALLATION *****/
-if(!file_exists("config.php")) $need_install = true;
-else require_once("config.php");
+if(!file_exists("config.php"))
+  $need_install = true;
+else
+  require_once("config.php");
 
 /***** CHECK FOR REQUIRED FILES/DIRECTORIES *****/
 if(!$need_install) {
-// only check for these paths if we're already installed
+
   $required_paths = array("admin/", "admin/index.php", "templates/", "posts/", "docs/", "functions.php");
   $success = true;
+
   foreach($required_paths as $path) {
+
     if(!file_exists($path)) {
+
       error("File or directory <tt>$path</tt> is missing.", false);
       $success = false;
+
     }
+
   }
+
   if(!$success)
     exit("<br />Either remove <tt>config.php</tt> to perform a fresh install, or replace any missing files/directories above.");
+
 }
 
 /***** HEADER *****/
 if($need_install)
   exit("<span style=\"font-family: trebuchet ms; font-size: 14px;\">It looks like you haven't installed kure yet!<br />Proceed to <a href=\"install.php\">installation</a> if you need to install.<br />If you don't, be sure to make sure your Kure-related directories exist.</span>\n");
-else runtemplate("header");
+else
+  runtemplate("header");
 
 
 /***** DISPLAY **************************************************************************/
 
 /***** DOCS *****/
 if(isset($_GET['docs'])) { // if we're at the docs page
+
   runtemplate("doclist_header");
   $alldocs = glob("docs/*.txt");
-  if(!$config['abcdocs']) { // if we're NOT sorting alphabetically
+
+  if(!$config['abcdocs']) // if we're NOT sorting alphabetically
     usort($alldocs, "sort_by_mtime");
-  }
+
   if(count($alldocs) == 0) {
+
     print("<i>&lt;no docs to display&gt;</i>");
+
   } else {
+
     foreach($alldocs as $file) {
+
       $title = $file;
       $title = str_replace("docs/", "", $title);
       $title = str_replace(".txt", "", $title);
@@ -86,25 +102,39 @@ if(isset($_GET['docs'])) { // if we're at the docs page
       $id = md5($title); // dynamic hook identifier
       $title = deparse_title($title);
       runtemplate("doclist", array("id" => $id, "DOCTITLE" => $title, "DOCADDRESS" => $uftitle, "DOCDATE" => date("F jS, Y", filemtime($file))));
+
     }
+
   }
+
   runtemplate("doclist_footer");
+
 }
 
 /***** VIEWPOST/VIEWDOC *****/
 elseif(isset($_GET['post']) || isset($_GET['doc'])) { // if a post/doc has been requested
+
   if(isset($_GET['post'])) {
+
     $type = "post";
     $filename = $_GET['post'];
+
   } else {
+
     $type = "doc";
     $filename = $_GET['doc'];
+
   }
+
   $filename = sanitize($filename);
   print plug($type, "top");
+
   if(!file_exists($type . "s/" . $filename . ".txt")) {
+
     print("The requested file <tt>" . $type . "s/" . $filename . ".txt</tt> does not exist.\n");
+
   } else {
+
     $file = $type . "s/" . $filename . ".txt";
     $title = $file;
     $title = str_replace($type . "s/", "", $title);
@@ -114,36 +144,52 @@ elseif(isset($_GET['post']) || isset($_GET['doc'])) { // if a post/doc has been 
     $content = str_replace("\n", "<br>\n", file_get_contents($file));
     
     runtemplate("entry", array("ENTRYTYPE" => $type, "ENTRYTITLE" => $title, "ENTRYADDRESS" => $uftitle, "ENTRYDATE" => date("F jS, Y", filemtime($file)), "ENTRYCONTENT" => $content));
-	}
+
+  }
+
 }
 
 /***** POSTS *****/
 else { // if we weren't told to do anything else
+
   runtemplate("postlist_header");
+
   if(!isset($_GET['page']))
     $_GET['page'] = 1; // default to page 1
+
   $allposts = glob("posts/*.txt");
   $total_posts = count($allposts);
-  if($total_posts % $config['num_posts'] != 0) {
+
   // if the total number of posts isn't divisible by the number we want to display,
   // then we want to make $total_posts / $config['num_posts'] round up one. (think it out.) this is for pagination.
+  if($total_posts % $config['num_posts'] != 0)
     $total_posts += $config['num_posts'];
-  }
+  
   if(!$config['abcposts']) // if we're NOT sorting alphabetically
     usort($allposts, "sort_by_mtime");
+
   $page_firstpost = ($_GET['page'] * $config['num_posts']) - $config['num_posts'];
   $curpost = 0;
   $i = 0; // monitor how many posts we display
+
   if(count($allposts) == 0) {
+
     print("<i>&lt;no posts to display&gt;</i>");
+
   } else {
-  foreach($allposts as $file) {
+
+    foreach($allposts as $file) {
+
       if($i == $config['num_posts'] && $config['num_posts'] != 0)
         break;
+
       if(isset($_GET['page']) && ($curpost < $page_firstpost) || ($curpost > ($page_firstpost + $config['num_posts']))) {
+
         $curpost++;
         continue;
+
       }
+
       $title = $file;
       $title = str_replace("posts/", "", $title);
       $title = str_replace(".txt", "", $title);
@@ -155,27 +201,36 @@ else { // if we weren't told to do anything else
       runtemplate("postlist", array("id" => $id, "POSTTITLE" => $title, "POSTADDRESS" => $uftitle, "POSTDATE" => date("F jS, Y", filemtime($file)), "POSTCONTENT" => $content));
       
       $i++;
+
     }
+
   }
 
   if($config['num_posts'] != 0 && $total_posts > $config['num_posts']) {
-    if($_GET['page'] + 1 <= $total_posts / $config['num_posts']) {
+
+    if($_GET['page'] + 1 <= $total_posts / $config['num_posts'])
       print("<a class=\"navitem\" href=\"?page=" . ($_GET['page'] + 1) . "\"><font size=\"1\">&lt;&lt;</font>previous posts</a>\n");
-    }
+    
     if($_GET['page'] != 1) {
-      if($_GET['page'] == 2) {
+
       // we check if we're on page 2, because the next page will be 1, aka homepage.
+      if($_GET['page'] == 2)
         $next = "./";
-      } else {
+      else
         $next = "?page=" . ($_GET['page'] - 1);
-      }
+
       print("| <a class=\"navitem\" href=\"" . $next . "\">more recent posts<font size=\"1\">&gt;&gt;</font></a>\n");
+
     }
+
   }
+
   runtemplate("postlist_footer");
+
 }
 
 /***** DISASSEMBLY *****/
 if(!isset($_GET['install']) && !$need_install) // if we're not installing
   runtemplate("footer");
+
 ?>
