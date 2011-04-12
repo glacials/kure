1f376d0507ac0f45add54cd376307bab820a2a4b
skoh-fley <skoh.fley@gmail.com>
2011-04-12 14:45:02 -0700

    started changes to migrate config to an object

---
+++ classes/config.php
@@ -0,0 +1,25 @@
+<?php
+
+class Config {
+
+  public static $version;
+
+  public static $adminPassword;
+  public static $showAdminLink;
+
+  public static $blogName;
+  public static $blogSub;
+
+  public static $template;
+
+  public static $postsPerPage;
+
+  public static $showDocDates;
+  public static $showDocPageDates;
+
+  public static $abcPosts;
+  public static $abcDocs;
+
+};
+
+?>
---
+++ classes/engine.php
@@ -0,0 +1,15 @@
+<?php
+
+class Engine {
+
+  $configFile;
+
+  __construct($configFile) {
+
+    require_once($configFile);
+
+  }
+
+};
+
+?>
