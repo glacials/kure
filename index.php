e980fe1b2d03bece5be0e3d63ef54786b501b1e0
skoh-fley <skoh.fley@gmail.com>
2011-04-13 17:04:29 -0700

    replaced some " with some

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
  */
 
 $need_install = false;
-$root = "./"; // so functions.php knows where we are
+$root = './'; // so functions.php knows where we are
 @include_once 'functions.php'; // supress errors because the file & directory check below will handle it
 
 require_once 'classes/config.php';
@@ -74,7 +74,7 @@ if(!$need_install) {
 if($need_install)
   exit('<span style="font-family: trebuchet ms; font-size: 14px;">It looks like you haven\'t installed kure yet!<br/>Proceed to <a href="install.php">installation</a> if you need to install.<br/>If you don\'t, be sure to make sure your Kure-related directories exist.</span>');
 else
-  runtemplate("header");
+  runtemplate('header');
 
 
 /***** DISPLAY **************************************************************************/
@@ -82,33 +82,33 @@ else
 /***** DOCS *****/
 if(isset($_GET['docs'])) { // if we're at the docs page
 
-  runtemplate("doclist_header");
-  $alldocs = glob("docs/*.txt");
+  runtemplate('doclist_header');
+  $alldocs = glob('docs/*.txt');
 
   if(!Config::$abcdocs) // if we're NOT sorting alphabetically
-    usort($alldocs, "sort_by_mtime");
+    usort($alldocs, 'sort_by_mtime');
 
   if(count($alldocs) == 0) {
 
-    print("<i>&lt;no docs to display&gt;</i>");
+    print('<i>&lt;no docs to display&gt;</i>');
 
   } else {
 
     foreach($alldocs as $file) {
 
       $title = $file;
-      $title = str_replace("docs/", "", $title);
-      $title = str_replace(".txt", "", $title);
+      $title = str_replace('docs/', '', $title);
+      $title = str_replace('.txt', '', $title);
       $uftitle = $title;
       $id = md5($title); // dynamic hook identifier
       $title = deparse_title($title);
-      runtemplate("doclist", array("id" => $id, "DOCTITLE" => $title, "DOCADDRESS" => $uftitle, "DOCDATE" => date("F jS, Y", filemtime($file))));
+      runtemplate('doclist', array('id' => $id, 'DOCTITLE' => $title, 'DOCADDRESS' => $uftitle, 'DOCDATE' => date('F jS, Y', filemtime($file))));
 
     }
 
   }
 
-  runtemplate("doclist_footer");
+  runtemplate('doclist_footer');
 
 }
 
@@ -117,34 +117,34 @@ elseif(isset($_GET['post']) || isset($_GET['doc'])) { // if a post/doc has been 
 
   if(isset($_GET['post'])) {
 
-    $type = "post";
+    $type = 'post';
     $filename = $_GET['post'];
 
   } else {
 
-    $type = "doc";
+    $type = 'doc';
     $filename = $_GET['doc'];
 
   }
 
   $filename = sanitize($filename);
-  print plug($type, "top");
+  print plug($type, 'top');
 
-  if(!file_exists($type . "s/" . $filename . ".txt")) {
+  if(!file_exists($type . 's/' . $filename . '.txt')) {
 
-    print("The requested file <tt>" . $type . "s/" . $filename . ".txt</tt> does not exist.\n");
+    print('The requested file <tt>' . $type . 's/' . $filename . '.txt</tt> does not exist.');
 
   } else {
 
-    $file = $type . "s/" . $filename . ".txt";
+    $file = $type . 's/' . $filename . '.txt';
     $title = $file;
-    $title = str_replace($type . "s/", "", $title);
-    $title = str_replace(".txt", "", $title);
+    $title = str_replace($type . 's/', '', $title);
+    $title = str_replace('.txt', '', $title);
     $uftitle = $title;
-    $title = str_replace("_", " ", $title);
-    $content = str_replace("\n", "<br>\n", file_get_contents($file));
+    $title = str_replace('_', ' ', $title);
+    $content = str_replace('\n', '<br/>', file_get_contents($file));
     
-    runtemplate("entry", array("ENTRYTYPE" => $type, "ENTRYTITLE" => $title, "ENTRYADDRESS" => $uftitle, "ENTRYDATE" => date("F jS, Y", filemtime($file)), "ENTRYCONTENT" => $content));
+    runtemplate('entry', array('ENTRYTYPE' => $type, 'ENTRYTITLE' => $title, 'ENTRYADDRESS' => $uftitle, 'ENTRYDATE' => date('F jS, Y', filemtime($file)), 'ENTRYCONTENT' => $content));
 
   }
 
@@ -153,12 +153,12 @@ elseif(isset($_GET['post']) || isset($_GET['doc'])) { // if a post/doc has been 
 /***** POSTS *****/
 else { // if we weren't told to do anything else
 
-  runtemplate("postlist_header");
+  runtemplate('postlist_header');
 
   if(!isset($_GET['page']))
     $_GET['page'] = 1; // default to page 1
 
-  $allposts = glob("posts/*.txt");
+  $allposts = glob('posts/*.txt');
   $total_posts = count($allposts);
 
   // if the total number of posts isn't divisible by the number we want to display,
@@ -167,7 +167,7 @@ else { // if we weren't told to do anything else
     $total_posts += Config::$num_posts;
   
   if(!Config::$abcposts) // if we're NOT sorting alphabetically
-    usort($allposts, "sort_by_mtime");
+    usort($allposts, 'sort_by_mtime');
 
   $page_firstpost = ($_GET['page'] * Config::$num_posts) - Config::$num_posts;
   $curpost = 0;
@@ -175,7 +175,7 @@ else { // if we weren't told to do anything else
 
   if(count($allposts) == 0) {
 
-    print("<i>&lt;no posts to display&gt;</i>");
+    print('<i>&lt;no posts to display&gt;</i>');
 
   } else {
 
@@ -192,14 +192,14 @@ else { // if we weren't told to do anything else
       }
 
       $title = $file;
-      $title = str_replace("posts/", "", $title);
-      $title = str_replace(".txt", "", $title);
+      $title = str_replace('posts/', '', $title);
+      $title = str_replace('.txt', '', $title);
       $uftitle = $title;
       $id = md5($title); // dynamic hook identifier
       $title = deparse_title($title);
-      $content = str_replace("\n", "<br>\n", file_get_contents($file));
+      $content = str_replace("\n", '<br/>' . "\n", file_get_contents($file));
       
-      runtemplate("postlist", array("id" => $id, "POSTTITLE" => $title, "POSTADDRESS" => $uftitle, "POSTDATE" => date("F jS, Y", filemtime($file)), "POSTCONTENT" => $content));
+      runtemplate('postlist', array('id' => $id, 'POSTTITLE' => $title, 'POSTADDRESS' => $uftitle, 'POSTDATE' => date('F jS, Y', filemtime($file)), 'POSTCONTENT' => $content));
       
       $i++;
 
@@ -210,28 +210,28 @@ else { // if we weren't told to do anything else
   if(Config::$num_posts != 0 && $total_posts > Config::$num_posts) {
 
     if($_GET['page'] + 1 <= $total_posts / Config::$num_posts)
-      print("<a class=\"navitem\" href=\"?page=" . ($_GET['page'] + 1) . "\"><font size=\"1\">&lt;&lt;</font>previous posts</a>\n");
+      print('<a class="navitem" href="?page=' . ($_GET['page'] + 1) . '"><font size="1">&lt;&lt;</font>previous posts</a>');
     
     if($_GET['page'] != 1) {
 
       // we check if we're on page 2, because the next page will be 1, aka homepage.
       if($_GET['page'] == 2)
-        $next = "./";
+        $next = './';
       else
-        $next = "?page=" . ($_GET['page'] - 1);
+        $next = '?page=' . ($_GET['page'] - 1);
 
-      print("| <a class=\"navitem\" href=\"" . $next . "\">more recent posts<font size=\"1\">&gt;&gt;</font></a>\n");
+      print('| <a class="navitem" href="' . $next . '">more recent posts<font size="1">&gt;&gt;</font></a>');
 
     }
 
   }
 
-  runtemplate("postlist_footer");
+  runtemplate('postlist_footer');
 
 }
 
 /***** DISASSEMBLY *****/
 if(!isset($_GET['install']) && !$need_install) // if we're not installing
-  runtemplate("footer");
+  runtemplate('footer');
 
 ?>
